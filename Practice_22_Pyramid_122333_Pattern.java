3ea26ac00370db7561daf5a2d024ebae5c530108
KashishRathore <kashishrathore0111@gmail.com>
2024-01-25 01:07:16 +0530

    New Commit

---
+++ Practice_22_Pyramid_122333_Pattern.java
@@ -0,0 +1,17 @@
+public class Practice_22_Pyramid_122333_Pattern {
+    public static void main(String[] args) {
+        int n = 4 ;
+          int z = 1 ;
+
+          for(int i = 1 ; i <=n ; i++){
+            for(int j = n-1 ; j >= i ; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1 ; k <= z ; k++){
+               System.out.print(i);
+            }
+            z += 2 ;
+            System.out.println();
+          }
+    }
+}
